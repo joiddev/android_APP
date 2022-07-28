124cced7c99b6d9c342687aff1fcf50f37132d08
unknown <onlyjoid2022@gmail.com>
2022-07-28 20:12:59 +0700

    StetelessWidget

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,24 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  var app = MaterialApp(
-    title: "My App",
-    home: Scaffold(
-      appBar: AppBar(
-        title: Text("APJ"),
-      ),
-      body: Text("Hello"),
-    ),
-    theme: ThemeData(primarySwatch: Colors.green),
-  );
+  runApp(MyApp());
+}
 
-  runApp(app);
+// Create Widget 
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: "My App",
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("APJ"),
+        ),
+        body: Text("Hello"),
+      ),
+      theme: ThemeData(primarySwatch: Colors.green),
+    );
+  }
 }
