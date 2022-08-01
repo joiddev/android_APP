f4c7c3a01e8d4c26c0b6d98fc3ba1c112ed7dc30
unknown <onlyjoid2022@gmail.com>
2022-08-02 00:15:13 +0700

    Container Widget

--- lib/main.dart
+++ lib/main.dart
@@ -28,35 +28,37 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
-  //กลุ่มข้อมูล
-  List<FoodMenu> menu = [
-    FoodMenu("hotdog", "500", "assets/images/hotdog.png"),
-    FoodMenu("sandwich", "2000", "assets/images/sandwich.png")
-  ];
-
   //แสดงผลข้อมูล
   @override
   Widget build(BuildContext context) {
     return Scaffold(
         appBar: AppBar(
-          title: Text("เลือกเมนูอาหาร"),
+          title: Text("บัญชีของฉัน"),
         ),
-        body: ListView.builder(
-            itemCount: menu.length,
-            itemBuilder: (BuildContext context, int index) {
-              FoodMenu food = menu[index];
-              return ListTile(
-                leading: Image.asset(food.img),
-                title: Text(
-                  food.name,
-                  style: TextStyle(fontSize: 30),
-                ),
-                subtitle: Text("ราคา" + food.price + " บาท"),
-                onTap: (){
-                  print("คุณเลือกเมนู "+food.name);
-                },
-              );
-            }));
+        body: Column(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                  color: Colors.limeAccent,
+                  borderRadius: BorderRadius.circular(10)),
+              height: 150,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.blueGrey,
+                borderRadius: BorderRadius.circular(10)
+              ),
+              height: 150,
+            ),
+            Container(
+              decoration: BoxDecoration(
+                color: Colors.deepOrange,
+                borderRadius: BorderRadius.circular(10)
+              ),
+              height: 150,
+            )
+          ],
+        ));
   }
 }
 
