8963b6bbcb6314bbf960106bc63db9d88b225709
unknown <onlyjoid2022@gmail.com>
2022-08-02 00:02:20 +0700

    onTap

--- lib/main.dart
+++ lib/main.dart
@@ -29,7 +29,7 @@ class MyHomePage extends StatefulWidget {
 
 class _MyHomePageState extends State<MyHomePage> {
   //กลุ่มข้อมูล
-  List<FoodMenu> menu=[
+  List<FoodMenu> menu = [
     FoodMenu("hotdog", "500", "assets/images/hotdog.png"),
     FoodMenu("sandwich", "2000", "assets/images/sandwich.png")
   ];
@@ -42,15 +42,21 @@ class _MyHomePageState extends State<MyHomePage> {
           title: Text("เลือกเมนูอาหาร"),
         ),
         body: ListView.builder(
-          itemCount: menu.length,
-          itemBuilder: (BuildContext context, int index) {
-            FoodMenu food=menu[index];
-            return ListTile(
-              leading: Image.asset(food.img),
-            title: Text(food.name, style: TextStyle(fontSize: 30),),
-            subtitle: Text("ราคา"+food.price+" บาท"),
-          );
-        }));
+            itemCount: menu.length,
+            itemBuilder: (BuildContext context, int index) {
+              FoodMenu food = menu[index];
+              return ListTile(
+                leading: Image.asset(food.img),
+                title: Text(
+                  food.name,
+                  style: TextStyle(fontSize: 30),
+                ),
+                subtitle: Text("ราคา" + food.price + " บาท"),
+                onTap: (){
+                  print("คุณเลือกเมนู "+food.name);
+                },
+              );
+            }));
   }
 }
 
